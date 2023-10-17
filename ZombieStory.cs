b186be1c371ed4d5780be4c7a4e71a7edcd49f81
serenaponse <41026248+serenaponse@users.noreply.github.com>
2023-10-17 12:38:19 -0700

    Add files via upload

---
+++ ZombieStory.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("There once was a zombie named: ");
+
+Console.ReadLine(); //zombie name
+
+Console.WriteLine("..they lived alone in a towned called ");
+
+Console.ReadLine(); //towname
+
+Console.WriteLine("..where no other zombies lived.\nBut one day the zombie saw a cat the size of a ");
+
+Console.ReadLine(); //cat size
+
+Console.WriteLine("Regardless of the cats size, they became friends");
+
+Console.ForegroundColor = ConsoleColor.Red; //changes tect color to red
+
+Console.WriteLine("They were no longer alone, and forever they remained friends.");
